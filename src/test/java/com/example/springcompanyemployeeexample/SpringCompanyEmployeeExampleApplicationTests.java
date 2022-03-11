cbf6d93270f4a4ed759765925f8a7506f609741a
rafpahlevanyan <rafok3nt@gmail.com>
2022-03-12 01:08:00 +0400

    initial commit

---
+++ src/test/java/com/example/springcompanyemployeeexample/SpringCompanyEmployeeExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcompanyemployeeexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCompanyEmployeeExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
